201e1e5895a5b0d0f70cab7f5825b04794452b99
Mayokun Areola <kareola960@gmail.com>
2025-11-09 02:44:39 +0100

    Text Correction

--- services/axios.ts
+++ services/axios.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const rawBaseURL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000";
-const baseURL = rawBaseURL.endsWith("/v1") ? rawBaseURL : `${rawBaseURL}/v1`;
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const Axios = axios.create({
   baseURL,
